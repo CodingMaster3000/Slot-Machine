7f90c25aa7d0812a911cc330b57444b04884c4e5
CodingMaster3000 <67920617+CodingMaster3000@users.noreply.github.com>
2026-05-23 19:02:12 +0200

    added cell filling code and added cell value bounds

--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@
         static void Main(string[] args)
         {
             const int BANKRUPTCY_CASH_LEVEL = 0;
+            const int CELL_VALUE_LOWER_BOUND_INCLUSIVE = 0;
+            const int CELL_VALUE_UPPER_BOUND_EXCLUSIVE = 2;
             int gridSize;
             int cash = 50;
+            int randomCellValue;
             string gridSizeUserInput;
             string mode;
             Console.WriteLine("You can choose the size of the grid for the slot machine. How large should the grid be?");
@@ -17,7 +20,20 @@
             while (cash > BANKRUPTCY_CASH_LEVEL)
             {
                 Console.WriteLine("Choose a mode.(center line / all horizontal lines / all vertical lines / all diagonal lines");
-                mode = Console.ReadLine();
+                mode = Console.ReadLine().ToLower();
+                for (int i = 0; i < gridSize; i++)
+                {
+                    for (int j = 0; j < gridSize; j++)
+                    {
+                        Random rng = new Random();
+                        randomCellValue = rng.Next(CELL_VALUE_LOWER_BOUND_INCLUSIVE, CELL_VALUE_UPPER_BOUND_EXCLUSIVE);
+                        grid[i,j] = randomCellValue.ToString();
+                        Console.Write($"{grid[i, j]}");
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+
             }
 
         }
