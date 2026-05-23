f595bfcc9f883b3c4a14cb5aa4ad5af91205aa19
CodingMaster3000 <67920617+CodingMaster3000@users.noreply.github.com>
2026-05-23 14:56:20 +0200

    introduced the grid and some communication with the user
    message asking the user for the grid size, the user input is read and used to create the griid, game start + request to choose a mode message

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int gridSize;
+            string gridSizeUserInput;
+            Console.WriteLine("You can choose the size of the grid for the slot machine. How large should the grid be?");
+            gridSizeUserInput = Console.ReadLine();
+            gridSize = Convert.ToInt32(gridSizeUserInput);
+            string[,] grid = new string[gridSize,gridSize];
+            Console.WriteLine("The game starts now.You have $50 cash at start.\n--------------------------------------------------");
+            Console.WriteLine("Choose a mode.(center line / all horizontal lines / all vertical lines / all diagonal lines");
+
+
         }
     }
 }
