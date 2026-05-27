e6e8b031567437d8a0af52def497a3f9b602ffa7
CodingMaster3000 <67920617+CodingMaster3000@users.noreply.github.com>
2026-05-27 22:03:08 +0200

    putting mode coordinates into arrays
    calculating amount of coordinates per mode, creating arrays for each mode with the calculated size, putting the coordinates into each mode array

--- Program.cs
+++ Program.cs
@@ -10,16 +10,45 @@
             int gridSize;
             int cash = 50;
             int randomCellValue;
-            int cemterLinePosition;
+            int centerLinePosition;
             int lineComboValue;
+            int amountOfCenterLineCoordinates;
+            int amountOfAllHorizontalCoordinates;
+            int amountOfAllVerticalCoordinates;
+            int amountOfAlldiagnoalsCoordniates;
             string gridSizeUserInput;
             string mode;
             bool lineWon;
             Console.WriteLine("You can choose the size of the grid for the slot machine. How large should the grid be?");
             gridSizeUserInput = Console.ReadLine();
             gridSize = Convert.ToInt32(gridSizeUserInput);
-            cemterLinePosition = gridSize / 2;
-            string[,] grid = new string[gridSize,gridSize];
+            amountOfCenterLineCoordinates = gridSize * 2;
+            amountOfAllHorizontalCoordinates = gridSize * gridSize * 2;
+            amountOfAllVerticalCoordinates = gridSize * gridSize * 2;
+            amountOfAlldiagnoalsCoordniates = gridSize * 2 * 2;
+            centerLinePosition = gridSize / 2;
+            string[] centerLine = new string[amountOfCenterLineCoordinates];
+            string[] allHorizontal = new string[amountOfAllHorizontalCoordinates];
+            string[] allVertical = new string[amountOfAllVerticalCoordinates];
+            string[] diagonals = new string[amountOfAlldiagnoalsCoordniates];
+            for (int i = 0; i < gridSize; i++)
+            {
+                centerLine[i * 2] = i.ToString();
+                centerLine[i * 2 + 1] = centerLinePosition.ToString();
+                for (int j = 0; j < gridSize; j++)
+                {
+                    allHorizontal[(i*gridSize + j)*2] = j.ToString();
+                    allHorizontal[(i * gridSize + j) * 2+1] = i.ToString();
+                    allVertical[(i * gridSize + j) * 2] = i.ToString();
+                    allVertical[(i * gridSize + j) * 2 + 1] = j.ToString();
+
+                }
+                diagonals[i] = i.ToString();
+                diagonals[i+1] = i.ToString();
+                diagonals[gridSize*2-i*2] = i.ToString();
+                diagonals[i*2 + 1] = i.ToString();
+            }
+            string[,] grid = new string[gridSize, gridSize];
             Console.WriteLine("The game starts now.You have $50 cash at start.\n--------------------------------------------------");
             while (cash > BANKRUPTCY_CASH_LEVEL)
             {
@@ -29,7 +58,7 @@
                     {
                         Random rng = new Random();
                         randomCellValue = rng.Next(CELL_VALUE_LOWER_BOUND_INCLUSIVE, CELL_VALUE_UPPER_BOUND_EXCLUSIVE);
-                        grid[i,j] = randomCellValue.ToString();
+                        grid[i, j] = randomCellValue.ToString();
                     }
                 }
                 Console.WriteLine("Choose a mode.(center line / all horizontal lines / all vertical lines / all diagonal lines");
@@ -37,10 +66,10 @@
                 if (mode == "center line")
                 {
                     lineWon = true;
-                    lineComboValue = Convert.ToInt32(grid[cemterLinePosition, 0]);
+                    lineComboValue = Convert.ToInt32(grid[centerLinePosition, 0]);
                     for (int i = 1; i < gridSize; i++)
                     {
-                        if (Convert.ToInt32(grid[cemterLinePosition,0]) != Convert.ToInt32(grid[cemterLinePosition, i]))
+                        if (Convert.ToInt32(grid[centerLinePosition, 0]) != Convert.ToInt32(grid[centerLinePosition, i]))
                         {
                             lineWon = false;
                         }
